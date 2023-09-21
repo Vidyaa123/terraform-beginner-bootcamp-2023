9e1c3a4e118ac48491cfbbb6a375aa3d7ae5ff5c
Vidyaa <mailvidyaa@gmail.com>
2023-09-21 12:10:11 +0100

    #13 connecting to terraform cloud backend (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "Vidyaa"
+
+    workspaces {
+      name = "Terra-Clicks"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
@@ -9,6 +16,11 @@ terraform {
       version = "5.17.0"
     }
   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  
 }
 
 provider "random" {

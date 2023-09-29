c8b74f8b3cb740ef27a1896734957f67ebd1449f
Vidyaa <mailvidyaa@gmail.com>
2023-09-29 15:12:59 +0100

    #23 removed random provider and added import stmt (#24)

--- main.tf
+++ main.tf
@@ -1,18 +1,13 @@
 
 
-resource "random_string" "bucket_name" {
-  length = 32
-  lower = true
-  upper = false
-  special = false
-}
 
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
 
-    #tags = {
-    #UserUid = var.user_uid
-    #Environment = "Dev"
-  #}
+resource "aws_s3_bucket" "Terra_Clicks_Bucket" {
+  bucket = var.bucket_name
+
+    tags = {
+    UserUid = var.user_uid
+    
+  }
 }
 
--- providers.tf
+++ providers.tf
@@ -7,10 +7,7 @@
 #     }
 #   }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"

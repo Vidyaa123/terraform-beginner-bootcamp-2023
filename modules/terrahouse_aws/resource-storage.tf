b435d0563e309bf44f760a2f1025ad38720184dd
Vidyaa <mailvidyaa@gmail.com>
2023-10-05 10:28:32 +0000

    #31 setting up content version

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -29,6 +29,10 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
   content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -36,7 +40,7 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.Terra_Clicks_Bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
-
+  content_type = "text/html"
   etag = filemd5(var.error_html_filepath)
 
 }
@@ -62,4 +66,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
